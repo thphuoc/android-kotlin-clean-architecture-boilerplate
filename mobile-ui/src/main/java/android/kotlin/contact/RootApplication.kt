26f2bee7ffb36f7d6b06f1b8583b456ff77323da
Phuoc Tran <tranhuuphuoc88@gmail.com>
2018-11-12 21:15:12 +0700

    Update

--- mobile-ui/src/main/java/android/kotlin/contact/RootApplication.kt
+++ mobile-ui/src/main/java/android/kotlin/contact/RootApplication.kt
@@ -3,10 +3,13 @@ package android.kotlin.contact
 import android.app.Activity
 import android.app.Application
 import android.kotlin.contact.injection.component.DaggerAppComponent
+import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
+import dagger.android.HasActivityInjector
+import timber.log.Timber
 import javax.inject.Inject
 
-class RootApplication: Application() {
+class RootApplication: Application(), HasActivityInjector {
     @Inject
     lateinit var activityDispatchingAndroidInjector: DispatchingAndroidInjector<Activity>
     override fun onCreate() {
