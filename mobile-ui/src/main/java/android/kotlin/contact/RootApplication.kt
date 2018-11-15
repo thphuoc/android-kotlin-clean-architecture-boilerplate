6ddef0193be6bd4204a71d0f6486cc516b24ad8f
PhuocTran <phuoc.tran@hubcba.com>
2018-11-15 18:05:10 +0700

    Update

--- mobile-ui/src/main/java/android/kotlin/contact/RootApplication.kt
+++ mobile-ui/src/main/java/android/kotlin/contact/RootApplication.kt
@@ -3,13 +3,14 @@ package android.kotlin.contact
 import android.app.Application
 import org.kodein.di.Kodein
 import org.kodein.di.KodeinAware
+import org.kodein.di.android.androidModule
 import timber.log.Timber
 
 
 class RootApplication : Application(), KodeinAware {
 
     override val kodein = Kodein.lazy {
-
+        import(androidModule(this@RootApplication))
     }
 
     override fun onCreate() {
