ad6543117a622a719cf1feacd26252e0fd40d275
Phuoc Tran <tranhuuphuoc88@gmail.com>
2019-05-05 13:54:30 +0700

    Update

--- domain/src/main/java/ride/customer/domain/entities/VehicleFare.kt
+++ domain/src/main/java/ride/customer/domain/entities/VehicleFare.kt
@@ -1,7 +1,7 @@
 package ride.customer.domain.entities
 
 data class VehicleFare (val name: String?="",
-                        val vehicleType: String?,
+                        val vehicleType: String?="",
                         val group: String?,
                         val iconFile: String?,
                         val baseFare: Double?,
